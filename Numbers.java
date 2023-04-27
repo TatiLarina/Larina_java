6e9e2e459a40b62aae29181ded64a7a44a4ee012
Tatyana Larina <jerrymurr@ya.ru>
2023-04-27 13:42:41 +0300

    reformat_code

--- Numbers.java
+++ Numbers.java
@@ -7,15 +7,15 @@ public class Numbers {
         Scanner input = new Scanner(System.in);
         System.out.print("Введите длину числового массива: ");
         int size = input.nextInt();
-        int array[] = new int[size];
+        int[] array = new int[size];
         System.out.print("Введите элементы числового массива: ");
         for (int i = 0; i < size; i++) {
             array[i] = input.nextInt();
         }
-        System.out.print ("Элементы массива, кратные 3: ");
+        System.out.print("Элементы массива, кратные 3: ");
         for (int i = 0; i < size; i++) {
-            if (array[i]%3 == 0) {
-                System.out.print (array[i] + " ");
+            if (array[i] % 3 == 0) {
+                System.out.print(array[i] + " ");
             }
         }
         System.out.println();
